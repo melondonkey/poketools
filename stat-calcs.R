e6d2e37e8692e2eca227a837508996791d702ac9
Joe Dean <joedean@joes-mac-mini.lan>
2022-09-02 10:32:27 -0400

    added a script to parse replays from showdown

--- stat-calcs.R
+++
@@ -1,9 +0,0 @@
-
-current_meta2 <- 
-  current_meta %>%
- # rowwise() %>%
-  mutate(
-    lv50_max_hp = ifelse(pokemon=='Shedinja', 1, hp_actual(base = baseStats_hp, IV = 31, EV = 252) ),
-    lv50_max_atk = stat_actual(baseStats_atk, EV = 252, nature = 1.2),
-    lv50_max_def = stat_actual(baseStats_def, EV = 252, nature = 1.2)
-  )
